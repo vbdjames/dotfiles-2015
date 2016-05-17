374702b655f5e24344fef73d055b690e9fdc41b6
Doug James <djames@mapcommunications.com>
2016-05-17 09:29:48 -0400

    updates

---
+++ hammerspoon/init.lua
@@ -0,0 +1,12 @@
+-- just a couple of examples
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "W", function()
+    hs.notify.new({title="Hammerspoon", informativeText="Hello world"}):send()
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "H", function()
+    local win = hs.window.focusedWindow()
+    local f = win:frame()
+
+    f.x = f.x - 10
+    win:setFrame(f)
+end)
